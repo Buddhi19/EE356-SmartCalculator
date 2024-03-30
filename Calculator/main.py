13781c08b26774cd02e2891f3fdc9dad8bcab17e
Buddhi Wijenayake <119914594+Buddhi19@users.noreply.github.com>
2024-03-30 07:28:14 +0000

    opencv integration failed

--- Calculator/main.py
+++ Calculator/main.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from model_load import for_test
 from PIL import Image, ImageTk
+import cv2
 
 
 def imresize(im,sz):
@@ -18,7 +19,25 @@ def resize( w_box, h_box, pil_image):
 	height = int(h*factor)    
 	return pil_image.resize((width, height), Image.ANTIALIAS)  
 
-img_test = Image.open("./test_images/test2.png").convert("L")
+
+class contours:
+	def __init__(self,img:np.array):
+		self.img = img
+
+	def preprocess(self):
+		self.gray = cv2.cvtColor(self.img, cv2.COLOR_BGR2GRAY)
+		self.blur = cv2.GaussianBlur(self.gray, (3,3), 0)
+		self.bw = cv2.threshold(self.blur, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
+
+	def plot_img(self):
+		plt.imshow(self.img)
+		plt.show()
+
+img_test = Image.open("./test_images/test3.png").convert("L")
+
+IMAGE = contours(img_test)
+IMAGE.preprocess()
+IMAGE.plot_img()
 
 img_test = np.invert(img_test)
 
