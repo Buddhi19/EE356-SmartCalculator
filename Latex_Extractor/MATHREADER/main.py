9ea5a6ba74bdea03d9afcd401d4f01a8a81c4d50
Buddhi19 <wijenayakebuddhi34802@gmail.com>
2024-04-11 23:05:35 +0530

    added library mathreader

---
+++ Latex_Extractor/MATHREADER/main.py
@@ -0,0 +1,4 @@
+from Libs.mathreader.mathreader import mathreader
+from Libs.mathreader.mathreader.api import *
+import Libs.mathreader.mathreader.config as Configuration
+
---
+++ Libs/mathreader/mathreader/classification/classification.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from mathreader.config import Configuration
+from mathreader import helpers
+from tensorflow.keras.models import load_model
+import tensorflow as tf
+import numpy as np
+import os
+
+config = Configuration()
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+os.environ['GLOG_minloglevel'] = '3'
+print('\n')
+
+
+def fit(image):
+
+    labels = helpers.get_labels()
+
+    try:
+        model = load_model(config.package_path + '/ann_models/model/model_11-07-2020_23-54-57.h5')
+
+        prediction = model.predict(image)
+        index = np.argmax(prediction)
+        print(index)
+        label_rec = labels["labels_parser"][str(index)]
+
+        return {
+            'label': labels["labels_recognition"][label_rec],
+            'prediction': prediction,
+            'type': 'not-number'
+        }
+
+    except Exception as e:
+        raise e
