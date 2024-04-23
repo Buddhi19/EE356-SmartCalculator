c7250962b1e792577d77a30f5a559fe8bc9f1411
Buddhi19 <wijenayakebuddhi34802@gmail.com>
2024-04-23 22:19:24 +0530

    working communicator

--- Communictor/Server/app.py
+++ Communictor/Server/app.py
@@ -4,10 +4,7 @@ import json
 
 app = Flask(__name__)
 
-RECEIVE_DATA = False
-SEND_DATA = True
-count = 0
-data_to_send = "hello from pc"
+host_url = '192.168.1.4'
 
 @app.route("/")
 def hello_world():
@@ -15,18 +12,20 @@ def hello_world():
 
 @app.route('/json1', methods=['GET','POST'])
 def handle_data():
-    if RECEIVE_DATA:
-        data = request.json
-        print(data)
-        return jsonify(data)
+    data = request.json
+    print(data)
+    return jsonify(data)
 
 @app.route('/json2', methods=['GET','POST'])
 def send_data():
-    if SEND_DATA:
+    f = open("toNodeMCU.txt", "r")
+    data_to_send = f.read()
+    f.close()
+    if data_to_send:
         send_data = {"data": data_to_send}
         return jsonify(send_data)
     else:
         return ""
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=80)
+    app.run(host=host_url,port=80)
---
+++ Communictor/toNodeMCU.py
@@ -0,0 +1,6 @@
+f = open("Server/toNodeMCU.txt", "w")
+
+# Write the data to the file
+
+f.write("you are doing great!")
+f.close()
