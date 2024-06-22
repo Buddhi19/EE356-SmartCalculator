6581bf5eb18befcaee861bbc78537b09b9479e87
Buddhi19 <wijenayakebuddhi34802@gmail.com>
2024-06-22 23:41:11 +0530

    added images

--- RaspberryPi/GUI/start_gui.py
+++ RaspberryPi/GUI/start_gui.py
@@ -9,11 +9,11 @@ class StartPage(tk.Frame):
     def __init__(self, parent, controller):
         super().__init__(parent)
         self.controller = controller
-        self.configure(bg="white")
+        self.configure(bg="#293C4A")
         self.create_widgets()
 
     def create_widgets(self):
-        label = tk.Label(self, text="Smart Calculator", bg="white", fg="#293C4A", font=("Arial", 30, "bold"))
+        label = tk.Label(self, text="Smart Calculator", bg="#293C4A", fg="white", font=("Arial", 30, "bold"))
         label.grid(row=0, column=0, columnspan=2, pady=20)
 
         buttons = [
@@ -34,8 +34,8 @@ class StartPage(tk.Frame):
                 photo = ImageTk.PhotoImage(image)
 
                 button = tk.Button(self, text=text, image=photo, compound="top", command=lambda name=frame_name: self.controller.show_frame(name),
-                                   font=("sans-serif", 10, "bold"), width=170, bg="white", borderwidth=0, highlightthickness=0,
-                                   fg="#293C4A")
+                                   font=("Arial", 10, "bold"), width=170, bg="#293C4A", borderwidth=0, highlightthickness=0,
+                                   fg="white")
                 button.image = photo  # keep a reference to the image
                 button.grid(row=(i//2) + 1, column=i % 2, pady=15, padx=10, sticky="nsew")
             except Exception as e:
